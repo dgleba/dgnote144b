6f79ea6bac3db9bf94f78b9a941871c4e01f76c0
dgleba <dgleba@gmail.com>
2014-03-30 23:26:44 -0400

    testing

--- modules/multipleautocomplete/actions/hello.php
+++ modules/multipleautocomplete/actions/hello.php
@@ -4,12 +4,12 @@ class actions_hello {
 	
 	    /*//file gets generated in app root, uncomment to see if function is running correctly
 	    $handle = fopen("tmp.txt", 'w');
-		fwrite($handle, "hello");
-		fclose($handle);
+		  fwrite($handle, "hello");
+		  fclose($handle);
 	    */
 		
         $hello = array("Hello World");
-		header('Content-type: application/json; charset="'.Dataface_Application::getInstance()->_conf['oe'].'"');
+		    //header('Content-type: application/json; charset="'.Dataface_Application::getInstance()->_conf['oe'].'"');
         echo json_encode($hello);
     }
 }
